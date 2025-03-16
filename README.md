917b3c4d82af0268830d27cfc3db1f4e1553bd6b
Chukseo <164390660+Chukseo@users.noreply.github.com>
2025-03-16 07:51:02 +0100

    navigate fix

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# React + Vite
+# React + Vite Blog App Template
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules with simple blog functionalities
 
 Currently, two official plugins are available:
 
--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,9 +1,26 @@
+// import { useEffect, useState } from "react";
+
+
 const Posts = () => {
+  // const [ setData] = useState([])
+  
+  // useEffect(() => {
+  //   fetch('http://localhost:8000/api/diarys/')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  //     .catch(e => console.log(e))
+  // })
+  
+
   return (
     <div className="row row-cols-1 row-cols-md-3 g-4">
       <div className="col">
         <div className="card h-100">
-          {/* <img src="..." className="card-img-top" alt="..."></img> */}
+          
           <div className="card-body">
             <h5 className="card-title">Moving our Communities Farward</h5>
             <p className="card-text">
